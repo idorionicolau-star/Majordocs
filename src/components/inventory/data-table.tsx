4ef2d4194e8b8912ab422243c708f801f3f8a205
Major Blogger <idorionicolau@gmail.com>
2026-01-07 11:00:31 +0000

    coloque sombra nisto (_for element <Button>_)

--- src/components/inventory/data-table.tsx
+++ src/components/inventory/data-table.tsx
@@ -90,7 +90,7 @@ export function InventoryDataTable<TData extends Product, TValue>({
             />
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button variant="outline" className="ml-auto">
+                    <Button variant="outline" className="ml-auto shadow-lg">
                         <ListFilter className="mr-2 h-4 w-4" />
                         Categoria
                         {selectedCategories.length > 0 && (
