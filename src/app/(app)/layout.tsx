bfd826f7dfa61c929ac774122a01e5983e85379c
Major Blogger <idorionicolau@gmail.com>
2026-01-08 05:49:24 +0000

    remova o subheader

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,10 @@
 
 import { Header } from "@/components/layout/header";
-import { SubHeader } from "@/components/layout/sub-header";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col bg-background">
       <Header />
-      <SubHeader />
       <main className="flex-1 p-4 sm:p-6 md:p-8 space-y-6 animate-in">
         {children}
       </main>
