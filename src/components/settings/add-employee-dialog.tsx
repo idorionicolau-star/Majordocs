94cc88d37046fd4c7270f6abf475ffa3299da929
Major Blogger <idorionicolau@gmail.com>
2026-01-09 13:28:16 +0000

    remova esta gestão de funcionários e adicione uma nova com base nas nova

--- src/components/settings/add-employee-dialog.tsx
+++ src/components/settings/add-employee-dialog.tsx
@@ -180,3 +180,5 @@ export function AddEmployeeDialog({ companyId }: AddEmployeeDialogProps) {
     </Dialog>
   );
 }
+
+    
--- src/components/settings/employee-manager.tsx
+++ src/components/settings/employee-manager.tsx
@@ -144,3 +144,5 @@ export function EmployeeManager({ companyId }: EmployeeManagerProps) {
     </>
   );
 }
+
+    
